610b4e95349d21aa6c176649c2f8f35b145fae82
ashitrj <88797325+ashitrj@users.noreply.github.com>
2023-11-03 20:24:07 +0530

    Delete BitCoinPriceChange directory

--- BitCoinPriceChange/bitcoin-client/src/main/java/com/ashit/bitcoinclient/kafka/PriceMessage.java
+++
@@ -1,7 +0,0 @@
-package com.ivanfranchin.bitcoinclient.kafka;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-public record PriceMessage(Long id, BigDecimal value, LocalDateTime timestamp) {
-}
